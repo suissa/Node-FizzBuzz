51233d8699d44259702630e8aba3f8f44a5d6aee
leocavalcante <lc@leocavalcante.com>
2012-07-30 19:10:08 -0300

    fixed jasmine test

--- test/jasmine/fizzbuzz-test-jasmine.js
+++ test/jasmine/fizzbuzz-test-jasmine.js
@@ -1,15 +1,18 @@
-var fizzbuzz = require( '../../lib/fizzbuzz-module.js' );
+var jasmine  = require( 'jasmine-node' ),
+	fizzbuzz = require( '../../lib/fizzbuzz-module.js' );
 
 describe( 'FizzBizz module', function () {
-    it( 'should return Fizz for divisibles of 3', function () {
-        expect( fizzbuzz.isFizz( 6 ) ).toBe( true );
+    it( 'should be Fizz for divisibles of 3', function () {
+        expect( fizzbuzz.isFizz( 6 ) ).toBe( 1 );
     });
     
-    it( 'should return Buzz for divisibles of 5', function () {
-        expect( fizzbuzz.isBuzz( 10 ) ).toBe( true );
+    it( 'should be Buzz for divisibles of 5', function () {
+        expect( fizzbuzz.isBuzz( 10 ) ).toBe( 1 );
     });
     
-    it( 'should return FizzBuzz for divisibles of 3 and 5', function () {
+    it( 'should be FizzBuzz for divisibles of 3 and 5', function () {
         expect( fizzbuzz.isFizzBuzz( 15 ) ).toBe( true );
     });
 });
+
+jasmine.executeSpecsInFolder( './' );
