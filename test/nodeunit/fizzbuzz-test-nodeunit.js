0e3b8be9e1e2a65f7287e389a48b2ada96be706f
Suissa <jean.nascimento@dafiti.com.br>
2012-07-30 16:09:27 -0300

    Nodeunit test for Buzz and FizzBuzz

--- test/nodeunit/fizzbuzz-test-nodeunit.js
+++ test/nodeunit/fizzbuzz-test-nodeunit.js
@@ -5,3 +5,11 @@ exports['Fizz'] = function (test) {
     test.equal(fizzbuzz.isFizz(6), true);
     test.done();
 };
+exports['Buzz'] = function (test) {
+    test.equal(fizzbuzz.isBuzz(10), true);
+    test.done();
+};
+exports['FizzBuzz'] = function (test) {
+    test.equal(fizzbuzz.isFizzBuzz(15), true);
+    test.done();
+};
