cdd0bb2526ddc3dbf61b072a0ab26433c1d84b0f
leocavalcante <lc@leocavalcante.com>
2012-08-01 17:16:02 -0300

    refactored tests and added stack trace

--- test/chai/fizzbuzz-test-chai.js
+++ test/chai/fizzbuzz-test-chai.js
@@ -4,19 +4,17 @@ var chai     = require( 'chai' ),
     should   = chai.should(),
     fizzbuzz = require( '../../lib/fizzbuzz-module' );
 
-// assert
+chai.Assertion.includeStack = true;
+
 assert.equal( fizzbuzz.isFizz( 3 ), 1, '3 is Fizz' );
 assert.equal( fizzbuzz.isBuzz( 5 ), 1, '5 is Buzz' );
 assert.equal( fizzbuzz.isFizzBuzz( 15 ), true, '15 is FizzBuzz' );
 
-
-// expect
-expect( fizzbuzz.isFizz( 3 ) ).to.be.equal( 1 );
-expect( fizzbuzz.isBuzz( 5 ) ).to.be.equal( 1 );
+expect( fizzbuzz.isFizz( 3 ) ).to.be.equal( true );
+expect( fizzbuzz.isBuzz( 5 ) ).to.be.equal( true );
 expect( fizzbuzz.isFizzBuzz( 15 ) ).to.be.equal( true );
 
-
-// should
-fizzbuzz.isFizz( 3 ).should.equal( 1 );
-fizzbuzz.isBuzz( 5 ).should.equal( 1 );
+fizzbuzz.isFizz( 3 ).should.equal( true );
+fizzbuzz.isBuzz( 5 ).should.equal( true );
 fizzbuzz.isFizzBuzz( 15 ).should.equal( true );
+
